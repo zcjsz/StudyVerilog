6b3f5601d428f9a99b416abc51fb4f05ca2a208c
Zhang <cjzhang@jv.tfme.com>
2019-06-06 23:59:43 +0800

    ex_case latch example

--- ex_case/design/ex_case.v
+++ ex_case/design/ex_case.v
@@ -18,7 +18,7 @@ always @(posedge sclk or negedge rst_n) begin
     end   
 end
 
-
+/*
 always @(posedge sclk or negedge rst_n) begin
     if(rst_n = 1'b0) begin
         o_data <= 8'd0;
@@ -45,7 +45,32 @@ always @(posedge sclk or negedge rst_n) begin
         endcase
     end
 end
+*/
 
+// 组合逻辑，消除锁存器的注意点
+// 1. 敏感列表要写全，比如下面的 case 语音的判断条件，还有赋值语句右边的变量，这些都要添加到敏感列表里面
+// 2. 所有条件分支要写全，比如 case 语句要加上 default
+always @(cnt_7) begin
+    case(cnt_7)
+        3'd0: begin 
+                o_data <= 3'd7;
+                o_dv <= 1'b1;
+                end
+        3'd1: begin
+                o_data <= 3'd0;
+                o_dv <= 1'b0;
+                end
+        3'd2: begin
+                o_data <= 3'd5;
+                o_dv <= 1'b1;
+                end
+        default: 
+                begin
+                o_data <= 3'd0;
+                o_dv <= 1'b0;
+                end
+    endcase
+end
 
 
 endmodule
